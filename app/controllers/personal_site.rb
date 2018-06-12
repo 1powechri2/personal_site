74ffdf6eaec42d64fb5c06e51675a13e3e5f598e
Chris Powell <chris.ian.powell@gmail.com>
2018-06-12 14:33:23 -0600

    adds .gitignore folder

--- app/controllers/personal_site.rb
+++ app/controllers/personal_site.rb
@@ -2,6 +2,7 @@ require 'rack'
 
 class PersonalSite
   def self.call(env)
-    ['200', {'Content-Type' => 'text/html'}, ['Welcome!']]
+    ['200', {'Content-Type' => 'text/html'},
+    ['Rock and Roll for Life!']]
   end
 end
--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -4,7 +4,8 @@ class HomepageTest < CapybaraTestCase
   def test_user_can_see_the_homepage
     visit '/'
 
-    assert page.has_content?("Welcome!")
+    save_and_open_page
+    assert page.has_content?('Rock')
     assert_equal 200, page.status_code
   end
 end
