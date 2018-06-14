17abe5011e066ec76068cfc6b98f1e5b04d1d14c
Chris Powell <chris.ian.powell@gmail.com>
2018-06-14 13:36:16 -0600

    basic page features completed

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -4,8 +4,30 @@ class HomepageTest < CapybaraTestCase
   def test_user_can_see_the_homepage
     visit '/'
 
-    save_and_open_page
-    assert page.has_content?('Rock')
+    assert page.has_content?('ROCK')
     assert_equal 200, page.status_code
+  end
+
+  def test_user_can_see_an_error_message
+    visit '/plop'
+
+    assert page.has_content?('Page not found.')
+    assert_equal 404, page.status_code
+  end
+
+  def test_user_can_see_rock
+    visit '/rock'
+
+    assert page.has_content?('ROCK!')
+    assert_equal 200, page.status_code
+  end
+
+  def test_rock_on_link
+    visit '/'
+    click_on "ROCK ON"
+
+    assert_equal 200, page.status_code
+    assert_equal '/rock', current_path
+    assert page.has_content?("ROCK!")
   end
 end
